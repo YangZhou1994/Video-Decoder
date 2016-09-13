8d7916ac335e8a2d5392bb60bef86c65fd129a2a
Ken Yu <kyu_115s@hotmail.com>
2016-09-13 23:17:14 +0800

    Add makefiles to Git.

---
+++ ReleaseTest/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lavutil -lavformat -lavcodec -lswscale -lopencv_core -lopencv_highgui -lopencv_imgproc
+
