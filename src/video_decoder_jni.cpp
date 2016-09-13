ba5ddd007404533a0372f1c034db284425edb373
Ken Yu <kyu_115s@hotmail.com>
2016-09-13 15:45:27 +0800

    Repair JNI.

--- src/video_decoder_jni.cpp
+++ src/video_decoder_jni.cpp
@@ -1,3 +1,4 @@
+#include <org_casia_cripac_isee_vpe_util_VideoDecoder.h>
 #include <cstdlib>
 #include <cstdio>
 #include <cstring>
@@ -5,63 +6,65 @@
 
 #include <jni.h>
 
-#include <org_cripac_isee_vpe_util_Decoder.h>
 #include <video_decoder.hpp>
 
 using namespace std;
-//using namespace videodecoder;
-//
-//JNIEXPORT jlong JNICALL Java_org_cripac_isee_vpe_util_Decoder_initialize(
-//		JNIEnv *env, jobject obj, jbyteArray buffer)
-//{
-//	jsize buffer_len = (env)->GetArrayLength(buffer);
-//	jbyte *contentdata = new jbyte[buffer_len];
-//	env->GetByteArrayRegion(buffer, 0, buffer_len, contentdata);
-//	VideoDecoder* p = new VideoDecoder((const unsigned char *) contentdata,
-//			buffer_len);
-//	return (jlong) p;
-//}
-//
-//JNIEXPORT jbyteArray JNICALL Java_org_cripac_isee_vpe_util_Decoder_nextFrame(
-//		JNIEnv *env, jobject obj, jlong pointer)
-//{
-//	VideoDecoder *p = (VideoDecoder *) pointer;
-//	unsigned char *frame_buf = new unsigned char[p->getFrameSize()];
-//	int ret = p->NextFrame(frame_buf);
-//	if (!ret)
-//		return NULL;
-//
-//	jbyteArray getbytes = (env)->NewByteArray(p->getFrameSize());
-//	env->SetByteArrayRegion(getbytes, 0, p->getFrameSize(),
-//			(const jbyte*) frame_buf);
-//	delete[] frame_buf;
-//	return getbytes;
-//}
-//
-//JNIEXPORT void JNICALL Java_org_cripac_isee_vpe_util_Decoder_free(JNIEnv *env,
-//		jobject obj, jlong pointer)
-//{
-//	delete (VideoDecoder *) pointer;
-//}
-//
-//JNIEXPORT jint JNICALL Java_org_cripac_isee_vpe_util_Decoder_getWidth(
-//		JNIEnv *env, jobject obj, jlong pointer)
-//{
-//	VideoDecoder *p = (VideoDecoder *) pointer;
-//	return p->get_width();
-//}
-//
-//JNIEXPORT jint JNICALL Java_org_cripac_isee_vpe_util_Decoder_getHeight(
-//		JNIEnv *env, jobject obj, jlong pointer)
-//{
-//	VideoDecoder *p = (VideoDecoder *) pointer;
-//	return p->get_height();
-//}
-//
-//JNIEXPORT jint JNICALL Java_org_cripac_isee_vpe_util_Decoder_getChannels(
-//		JNIEnv *env, jobject obj, jlong pointer)
-//{
-//	VideoDecoder *p = (VideoDecoder *) pointer;
-//	return p->getChannels();
-//}
+using namespace videodecoder;
+
+JNIEXPORT jlong JNICALL Java_org_casia_cripac_isee_vpe_util_VideoDecoder_initialize(
+		JNIEnv *env, jobject obj, jbyteArray buffer)
+{
+	jsize buffer_len = (env)->GetArrayLength(buffer);
+	jbyte *contentdata = new jbyte[buffer_len];
+	env->GetByteArrayRegion(buffer, 0, buffer_len, contentdata);
+	VideoDecoder* p = new VideoDecoder((const unsigned char *) contentdata,
+			buffer_len);
+	return (jlong) p;
+}
+
+JNIEXPORT jbyteArray JNICALL Java_org_casia_cripac_isee_vpe_util_VideoDecoder_nextFrame(
+		JNIEnv *env, jobject obj, jlong raw_pointer)
+{
+	VideoDecoder *p = (VideoDecoder *) raw_pointer;
+	unsigned char *frame_buf = new unsigned char[p->getFrameSize()];
+	int ret = p->NextFrame(frame_buf);
+	if (!ret)
+		return NULL;
+
+	jbyteArray getbytes = (env)->NewByteArray(p->getFrameSize());
+	env->SetByteArrayRegion(getbytes, 0, p->getFrameSize(),
+			(const jbyte*) frame_buf);
+	delete[] frame_buf;
+	return getbytes;
+}
+
+JNIEXPORT int JNICALL Java_org_casia_cripac_isee_vpe_util_VideoDecoder_skipFrame
+  (JNIEnv *env, jobject obj, jlong pointer, jint num_frames)
+{
+	return ((VideoDecoder *) pointer)->SkipFrame(num_frames);
+}
+
+JNIEXPORT void JNICALL Java_org_casia_cripac_isee_vpe_util_VideoDecoder_free(JNIEnv *env,
+		jobject obj, jlong pointer)
+{
+	delete (VideoDecoder *) pointer;
+}
+
+JNIEXPORT jint JNICALL Java_org_casia_cripac_isee_vpe_util_VideoDecoder_getWidth(
+		JNIEnv *env, jobject obj, jlong pointer)
+{
+	return ((VideoDecoder *) pointer)->get_width();
+}
+
+JNIEXPORT jint JNICALL Java_org_casia_cripac_isee_vpe_util_VideoDecoder_getHeight(
+		JNIEnv *env, jobject obj, jlong pointer)
+{
+	return ((VideoDecoder *) pointer)->get_height();
+}
+
+JNIEXPORT jint JNICALL Java_org_casia_cripac_isee_vpe_util_VideoDecoder_getChannels(
+		JNIEnv *env, jobject obj, jlong pointer)
+{
+	return ((VideoDecoder *) pointer)->getChannels();
+}
 
