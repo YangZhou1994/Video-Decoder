41e597dedcc4f53d5385d684fdacb1c8466d184b
Ken Yu <kyu_115s@hotmail.com>
2016-10-05 18:08:57 +0800

    Ignore cmake files. Rename Java decoder package.

--- jni/video_decoder_jni.cpp
+++ jni/video_decoder_jni.cpp
@@ -21,12 +21,12 @@
 #include <jni.h>
 
 #include <video_decoder.hpp>
-#include <org_casia_cripac_isee_vpe_util_VideoDecoder.h>
+#include <org_cripac_isee_vpe_util_VideoDecoder.h>
 
 using namespace std;
 using namespace isee;
 
-JNIEXPORT jlong JNICALL Java_org_casia_cripac_isee_vpe_util_VideoDecoder_initialize(
+JNIEXPORT jlong JNICALL Java_org_cripac_isee_vpe_util_VideoDecoder_initialize(
     JNIEnv *env, jobject obj, jbyteArray buffer) {
   jsize buffer_len = (env)->GetArrayLength(buffer);
   jbyte *contentdata = new jbyte[buffer_len];
@@ -42,7 +42,7 @@ JNIEXPORT jlong JNICALL Java_org_casia_cripac_isee_vpe_util_VideoDecoder_initial
   return (jlong) p;
 }
 
-JNIEXPORT jbyteArray JNICALL Java_org_casia_cripac_isee_vpe_util_VideoDecoder_nextFrame(
+JNIEXPORT jbyteArray JNICALL Java_org_cripac_isee_vpe_util_VideoDecoder_nextFrame(
     JNIEnv *env, jobject obj, jlong raw_pointer) {
   VideoDecoder *p = (VideoDecoder *) raw_pointer;
   unsigned char *frame_buf = new unsigned char[p->getFrameSize()];
@@ -57,27 +57,27 @@ JNIEXPORT jbyteArray JNICALL Java_org_casia_cripac_isee_vpe_util_VideoDecoder_ne
   return jFrameBytes;
 }
 
-JNIEXPORT int JNICALL Java_org_casia_cripac_isee_vpe_util_VideoDecoder_skipFrame
+JNIEXPORT int JNICALL Java_org_cripac_isee_vpe_util_VideoDecoder_skipFrame
     (JNIEnv *env, jobject obj, jlong pointer, jint num_frames) {
   return ((VideoDecoder *) pointer)->SkipFrame(num_frames);
 }
 
-JNIEXPORT void JNICALL Java_org_casia_cripac_isee_vpe_util_VideoDecoder_free(JNIEnv *env,
+JNIEXPORT void JNICALL Java_org_cripac_isee_vpe_util_VideoDecoder_free(JNIEnv *env,
                                                                              jobject obj, jlong pointer) {
   delete (VideoDecoder *) pointer;
 }
 
-JNIEXPORT jint JNICALL Java_org_casia_cripac_isee_vpe_util_VideoDecoder_getWidth(
+JNIEXPORT jint JNICALL Java_org_cripac_isee_vpe_util_VideoDecoder_getWidth(
     JNIEnv *env, jobject obj, jlong pointer) {
   return ((VideoDecoder *) pointer)->get_width();
 }
 
-JNIEXPORT jint JNICALL Java_org_casia_cripac_isee_vpe_util_VideoDecoder_getHeight(
+JNIEXPORT jint JNICALL Java_org_cripac_isee_vpe_util_VideoDecoder_getHeight(
     JNIEnv *env, jobject obj, jlong pointer) {
   return ((VideoDecoder *) pointer)->get_height();
 }
 
-JNIEXPORT jint JNICALL Java_org_casia_cripac_isee_vpe_util_VideoDecoder_getChannels(
+JNIEXPORT jint JNICALL Java_org_cripac_isee_vpe_util_VideoDecoder_getChannels(
     JNIEnv *env, jobject obj, jlong pointer) {
   return ((VideoDecoder *) pointer)->getChannels();
 }
--- test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ken/Documents/LaS-VPE Platform/Video-Decoder/test
-# Build directory: /home/ken/Documents/LaS-VPE Platform/Video-Decoder/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(video_decoder_test "/home/ken/Documents/LaS-VPE Platform/Video-Decoder/test/video_decoder_test")
-add_test(echo "echo" "1")
